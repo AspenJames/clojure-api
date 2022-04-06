fd32ca7b74c960b3af513cc2241baedccc595e12
Aspen James <aspenjames@tqca.org>
2022-04-05 18:16:34 -0700

    Setup REPL profile & add to .gitignore

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :main ^:skip-aot clojure-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :repl {:plugins [[cider/cider-nrepl "0.27.3"]]}})
