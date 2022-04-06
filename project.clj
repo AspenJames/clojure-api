bb0670ba5353d844f002b683e4c3b4a97e8fb352
Aspen James <aspenjames@tqca.org>
2022-04-05 22:29:29 -0700

    Setup basic API structure
    - Add compojure for routing
    - Add ring for req/res
    - Add ring/jetty for server
    - Add ring defaults for middlewares
    - Get port from CLI flag
    - Add `server` atom w/ start/stop functions
    - Setup server in `-main`

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.cli "1.0.206"] ; Parse args
+                 [compojure "1.6.2"]               ; Request/response routing
+                 [ring/ring-core "1.8.2"]          ; Request/response handling
+                 [ring/ring-defaults "0.3.3"]      ; Middleware defaults
+                 [ring/ring-jetty-adapter "1.8.2"] ; Run Jetty web server
+                 ]
   :main ^:skip-aot clojure-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/clojure_api/core.clj
+++ src/clojure_api/core.clj
@@ -1,7 +1,48 @@
 (ns clojure-api.core
-  (:gen-class))
+  "Example HTTP API."
+  (:gen-class)
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [compojure.core :refer [defroutes
+                                    GET]]
+            [compojure.route :refer [not-found]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.defaults :refer [api-defaults
+                                              wrap-defaults]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defonce server (atom nil))
+
+(defroutes app
+  (GET "/" [] "Placeholder")
+  (not-found "Route not found"))
+
+(defn run-server!
+  "Run the API server on the specified port."
+  [port]
+  (try
+    (reset! server (run-jetty (-> #'app
+                                  (wrap-defaults api-defaults))
+                              {:join? false
+                               :port port
+                               :send-server-version? false}))
+    (println "API listening on port" port)
+    (catch Exception e (println "ERROR:" (.getMessage e)))))
+
+(defn stop-server!
+  "Stop the API server, if running."
+  []
+  (when-not (nil? @server)
+    (.stop @server)
+    (reset! server nil)))
+
+(def cli-opts
+  [["-p" "--port PORT" "Port number"
+    :default 4200
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]])
+
+(defn -main [& args]
+  (let [options (parse-opts args cli-opts)
+        port (get-in options [:options :port])
+        errors (get options :errors nil)
+        _ (when-not (nil? errors) (apply println "ERROR:" errors))]
+    (run-server! port)))
